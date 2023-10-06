08bc4a858edb8380faacd7d635cf081a05e8f2a3
edanvaguPC <edward_avg@hotmail.com>
2023-10-06 14:30:01 -0500

    data script

--- Wiis/src/Clases/generateScriptInventory.java
+++ Wiis/src/Clases/generateScriptInventory.java
@@ -1,25 +1,46 @@
 package Clases;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Random;
 
 public class generateScriptInventory {
-    final int cuantity;
-    Random random = new Random();
-    final String[] names = {"Smartphone", "Laptop", "Tablet","Televisor",
-            "Camara", "Auriculares", "Altavoz", "Consola", "Smartwatch",
-            "Teclado", "Raton", "Impresora", "Disco_duro", "Router", "Monitor",
-            "Cargador", "Proyector", "Dron", "Tarjeta_Grafica", "Reloj_Inteligente"};
 
-    public generateScriptInventory(int cuantity) {
-        this.cuantity=cuantity;
-    }
-    public List<Product> generate(){
-        List<Product> products = new LinkedList<>();
-        for (int i = 0; i < cuantity; i++) {
-            products.add(new Product(String.valueOf(i),String.valueOf(i),i, i));
+    public static void main(String[] args) {
+        String folderPath = System.getProperty("user.dir") + File.separator + "Files";
+        String fileName = folderPath + "/prueba.txt";
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            Random random = new Random();
+
+            for (int i = 0; i < 1000; i++) {
+                String codigo = "P"+i; // Genera un código de 5 caracteres aleatorios
+                String nombre = "Product"+i; // Genera un nombre de 10 caracteres aleatorios
+                double precio = 10 + random.nextDouble() * 90; // Genera un precio entre 10 y 100
+                int cantidad = random.nextInt(100); // Genera una cantidad aleatoria entre 0 y 99
+
+                String linea = codigo + "," + nombre + "," + precio + "," + cantidad;
+                writer.write(linea);
+                writer.newLine(); // Agrega una nueva línea después de cada dato
+            }
+
+            System.out.println("Se generaron 1000 datos y se guardaron en " + fileName);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return products;
+    }
+
+    private static String generateRandomString(int length) {
+        String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder sb = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            int index = (int) (Math.random() * caracteres.length());
+            sb.append(caracteres.charAt(index));
+        }
+
+        return sb.toString();
     }
 }
